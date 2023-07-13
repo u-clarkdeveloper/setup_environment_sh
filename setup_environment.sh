420e3702a681952881851b0b5a239079df24c8ad
Jesse Clark <jesse@clarkdeveloper.com>
2023-07-13 09:40:30 -0500

    intit

---
+++ setup_environment.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+file_path="./.env"
+secret_list="./secret_list"
+required_env_vars="./required_env_vars"
+
+echo "Setting up environment with secrets from Hashicorp Vault Secrets."
+echo "--------------------------------------------------------------"
+echo ""
+
+
+
+continue_flag=1
+
+if [ -f "$file_path" ]; then
+    rm "$file_path"
+    touch "$file_path"
+else
+    touch "$file_path"
+    echo "File .env file created."
+    echo ""
+fi
+
+# Check if the required environment variables file exists
+if [ -f "$required_env_vars" ]; then
+    echo "Getting required environment variables from $required_env_vars"
+    echo "--------------------------------------------------------------"
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        # Process each item in the list
+        echo "Fetching Variable: $line"
+        if [[ -v line ]]; then
+            : # Do nothing
+        else
+            echo "Environment variable $line does not exist or is not set. Set it with the command"
+            echo "export $line=<$line>"
+            echo ""
+            continue_flag=0
+        fi
+    done < "$required_env_vars"
+    echo ""
+else
+    echo "File not found: $required_env_vars, Please create the file with the list of required environment variables."
+fi
+
+# Check if the secret list file exists
+if [[ continue_flag -eq 0 ]]; then
+    echo "Exiting script. Please set the environment variables and run the script again."
+    exit 1
+else
+    # Check if the file exists
+    if [ -f "$secret_list" ]; then
+        # Read the file line by line and loop over the list
+        echo "Getting secrets from $secret_list"
+        echo "--------------------------------------------------------------"
+        while IFS= read -r line || [[ -n "$line" ]]; do
+            # Process each item in the list
+            echo "Fetching Variable: $line"
+            value=$(vlt secrets get -plaintext $line)
+            if [ "${value:0:7}" == "Error: " ]; then
+                echo "Secret $line not found. Please check the secret name and try again."
+                echo ""
+            else
+                echo "$line=$value"
+                echo ""
+            fi
+        done < "$secret_list"
+    else
+        echo "File not found: $secret_list, Please create the file with the list of secrets to fetch."
+        echo ""
+    fi
+    echo "Done"
+    echo ""
+fi
+
