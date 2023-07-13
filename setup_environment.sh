02ebade9e2464d7d5b78ea00b9a593d296ca52a9
Jesse Clark <jesse@clarkdeveloper.com>
2023-07-13 10:18:11 -0500

    Bug Fix: env vars empty and remove existing .env file

--- setup_environment.sh
+++ setup_environment.sh
@@ -14,7 +14,7 @@ continue_flag=1
 if [ -f "$required_env_vars" ]; then
     echo "Getting required environment variables from $required_env_vars"
     echo "--------------------------------------------------------------"
-    if [ -s "$required_env_vars" ]: then
+    if [ ! -s "$required_env_vars" ]; then
         echo "INFO: File $required_env_vars is empty continuing to process secrets"
     fi
     while IFS= read -r line || [[ -n "$line" ]]; do
@@ -43,18 +43,19 @@ if [[ continue_flag -eq 0 ]]; then
 else
     # Check if the file exists
     if [ -f "$secret_list" ]; then
+        rm $file_path
         # Read the file line by line and loop over the list
         echo "Getting secrets from $secret_list"
         echo "--------------------------------------------------------------"
         while IFS= read -r line || [[ -n "$line" ]]; do
             # Process each item in the list
-            echo "Fetching Variable: $line"
+            echo "Fetching Secret: $line"
             value=$(vlt secrets get -plaintext $line)
             if [ "${value:0:7}" == "Error: " ]; then
                 echo "Secret $line not found. Please check the secret name and try again."
                 echo ""
             else
-                echo "$line=$value"
+                echo "$line=$value" >> $file_path
                 echo ""
             fi
         done < "$secret_list"
