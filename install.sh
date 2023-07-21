2ec15b6c1ce537faf07026ec480193c64329bf56
Jesse Clark <jesse.clark@davenportiowa.com>
2023-07-21 10:55:01 -0500

    Update: +x and comments

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ cat <<EOT >> ../setup_environment.sh
 source <(curl -s https://raw.githubusercontent.com/u-clarkdeveloper/setup_environment_sh/main/setup_environment.sh)
 EOT
 
+# move files over to repo
 mv ./secret_env_map ./secret_list ./required_env_vars ../
+
+# remove this repo
 cd ..
 rm -rf ./setup_environment_sh
